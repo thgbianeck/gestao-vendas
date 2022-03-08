ee9ce0528b1885941a4805a4716bbe5fd8f31c4b
Thiago Moreira Bianeck <thgbianeck@gmail.com>
2022-03-08 00:44:27 -0300

    first commit

---
+++ src/main/java/br/com/bnck/gvendas/gestaovendas/GestaoVendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.bnck.gvendas.gestaovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoVendasApplication.class, args);
+	}
+
+}
